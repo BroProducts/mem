9214dbc33db6f2332d1bb654fcb8ca4b00d1e84f
Wenish <jonas.voland@bluewin.ch>
2017-09-07 23:54:58 +0200

    update

---
+++ server/colyseus-vanilla-js-example/src/index.js
@@ -0,0 +1,17 @@
+let foo = () => console.log('Hi Node')
+/*
+var http = require('http')
+var port = process.env.PORT || 2657
+var express = require('express')
+var colyseus = require('colyseus')
+var ChatRoom = require('./rooms/chat-room')
+console.log(ChatRoom)
+var app = express()
+var server = http.createServer(app);
+var gameServer = new colyseus.Server({server: server})
+
+gameServer.register('Chat', ChatRoom)
+server.listen(port);
+
+console.log(`Listening on http://localhost:${ port }`)
+*/
---
+++ server/colyseus-vanilla-js-example/src/rooms/chat-room.js
@@ -0,0 +1,27 @@
+var Room = require('colyseus').Room
+
+class ChatRoom extends Room {
+      onInit (options) {
+          this.setState({ messages: [] });
+          console.log("ChatRoom created!", options);
+      }
+
+      onJoin (client) {
+          this.state.messages.push(`${ client.id } joined.`);
+      }
+
+      onLeave (client) {
+          this.state.messages.push(`${ client.id } left.`);
+      }
+
+      onMessage (client, data) {
+          this.state.messages.push(data.message);
+          console.log("ChatRoom:", client.id, data);
+      }
+
+      onDispose () {
+          console.log("Dispose ChatRoom");
+      }
+}
+
+module.exports = ChatRoom
