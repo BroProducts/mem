40fa13d958571004c5e4b8053428b17f8d35682c
Wenish <jonas.voland@bluewin.ch>
2017-09-08 13:18:27 +0200

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,8 +3,7 @@ import * as express from 'express';
 import { createServer } from 'http';
 import { Server } from 'colyseus';
 
-// Require ChatRoom handler
-import { ChatRoom } from "./rooms/01-basic";
+// Require Hub handler
 import { Hub } from "./rooms/hub";
 
 const port = Number(process.env.PORT || 2657);
@@ -16,8 +15,7 @@ const httpServer = createServer(app);
 // Attach WebSocket Server on HTTP Server.
 const gameServer = new Server({ server: httpServer });
 
-// Register ChatRoom as "chat"
-gameServer.register("chat", ChatRoom);
+// Register Hub as "hub"
 gameServer.register("hub", Hub);
 
 
